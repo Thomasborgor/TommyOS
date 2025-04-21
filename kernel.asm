e8189bd3386d54df832a1d292d46fbc38e60a6bd
Thomasborgor <toppertommy3@gmail.com>
2025-04-21 13:56:10 -0500

    optimizations

--- kernel.asm
+++ kernel.asm
@@ -70,7 +70,7 @@ main1:
 	mov dl, 37 ;set cursor again
 	int 0x10
 	mov si, msg2
-	call print
+	call os_print_string
 	
 	mov dh, 0x02
 	mov dh, 10
@@ -86,13 +86,13 @@ main:
 	after_load_auto:
 	xor ax, ax
 	mov si, kernel_ver
-	call print
+	call os_print_string
 	second:
 		mov ah, 5
 		mov al, 0
 		int 0x10
 		mov si, prompt
-		call print
+		call os_print_string
 		mov cx, 40
 		mov si, input_buffer
 		rst_loop:
@@ -111,17 +111,6 @@ main:
 		mov di, input_buffer_copy_copy
 		rep stosb
 		
-		mov cx, 60
-		mov al, 0
-		mov di, entire_command
-		rep stosb
-		
-		mov cx, 1024
-		mov si, dirlist
-		rst_loop3:
-			mov byte [si], 0x00
-			inc si
-			loop rst_loop3
 		
 		
 		lea di, [input_buffer]
@@ -150,7 +139,8 @@ backspace:
 	cmp dl, 2
 	jle input_loop
 	mov si, backspace_msg
-	call print
+	call os_print_string
+
 	dec di
 	jmp input_loop
 
@@ -161,10 +151,16 @@ parse: ;how we are going to do this:
 	dec di
 	do_it_thing:
 	mov byte [di], 0x00
+	
 	mov si, input_buffer
-	mov di, entire_command
-	call os_string_copy
-	cmp byte [si], 0
+	mov di, buffer
+	mov cx, 60
+	lopp:
+		lodsb
+		stosb
+	loop lopp
+	
+	cmp byte [input_buffer], 0
 	je second
 	
 	mov ax, input_buffer
@@ -242,13 +238,7 @@ parse: ;how we are going to do this:
 	jc echo_do
 	
 	;get the input, go to uppercase, figure out the length, 8-length is the number of spaces we need to pad, then add BIN to the end. 
-	mov di, txt_extension
-	call test_extension
-	jc no_run_txt
-	
-	mov di, tom_extension
-	call test_extension
-	jc no_run_tom
+
 	
 	mov di, pcx_extension
 	call test_extension
@@ -277,7 +267,7 @@ parse: ;how we are going to do this:
 	
 	unknown_command_place:
 	mov si, unkown_command
-	call print
+	call os_print_string
 	jmp second
 	
 boot_pcx db 'BOOT.PCX', 0
@@ -300,9 +290,9 @@ echo_do:
 	int 0x10
 	mov al, 0dh
 	int 0x10
-	mov si, entire_command
+	mov si, buffer
 	add si, 5
-	call print
+	call os_print_string
 	jmp second
 	
 test_extension:
@@ -324,6 +314,7 @@ test_extension:
 		ret
 	
 compare_string:
+	
 	pusha
 	mov si, input_buffer_copy
 	compare_loop:
@@ -418,7 +409,7 @@ ready_the_drive:
 no_drive:
 	popa
 	mov si, no_drive_msg
-	call print
+	call os_print_string
 
 	jmp second
 
@@ -440,21 +431,18 @@ yes_run_pcx:
 	jmp second
 
 	
-no_run_txt:
-	mov si, no_txt_msg
-	call print
-	jmp second
-	
-no_run_tom:
-	mov si, no_tom_msg
-	call print
-	jmp second
-	
+
 copy_do:
 	mov word [tmp_one], 0
 	mov byte [tmp_dev_one], 0
-	mov si, entire_command
-	add si, 5
+	mov si, input_buffer
+	add si, 7
+
+	mov di, input_buffer_copy
+	call os_string_copy
+	mov [tmp_one], cx
+	
+	sub si, 2
 	lodsb
 	cmp al, 65
 	jl unknown_command_place
@@ -464,6 +452,8 @@ copy_do:
 	jle is_a_or_b
 	add al, 61
 	mov [bootdev], al
+	
+	
 
 	
 	mov dl, al
@@ -477,30 +467,27 @@ copy_do:
 	mov [Sides], dx
 	call ready_the_drive
 	ready_for_param_seek:
+
+
 	cmp byte [si], ':'
 	jne unknown_command_place
-	inc si
-	mov di, input_buffer_copy
-	get_the_first_file:
-		inc byte [tmp_dev_one]
-		lodsb
-		cmp al, 32
-		je done_get_first_file
-		stosb
-		jmp get_the_first_file
-		
-	done_get_first_file:
+	mov si, input_buffer
+	add si, [tmp_one]
+	add si, 9
+	mov [tmp_one], si
+	
+	mov di, input_buffer_copy_copy
+	call os_string_copy
 
+	
 	mov ax, input_buffer_copy
 	mov cx, 32768
 	call os_load_file
-	jc no_file
+	mov si, [tmp_one]
+	sub si, 2
+	
 	mov [tmp_one], bx
-	mov si, entire_command
-	add si, 7
-	mov al, [tmp_dev_one]
-	xor ah, ah
-	add si, ax
+	jc no_file
 	lodsb
 	cmp al, 65
 	jl unknown_command_place
@@ -524,17 +511,6 @@ copy_do:
 	ready_for_param_seek2:
 	cmp byte [si], ':'
 	jne unknown_command_place
-	inc si
-	mov di, input_buffer_copy_copy
-	get_the_second_file:
-		lodsb
-		cmp al, 0
-		je done_get_last_file
-		stosb
-		jmp get_the_second_file
-	
-	done_get_last_file:
-	
 	
 	mov cx, [tmp_one]
 	mov bx, 32768
@@ -561,6 +537,7 @@ is_a_or_b:
 	mov byte [bootdev], 0
 	call ready_the_drive
 	jmp ready_for_param_seek
+	
 is_a_or_b_2:
 	mov word [SecsPerTrack], 18
 	mov word [Sides], 2
@@ -583,29 +560,18 @@ tmp_dev_two db 0
 
 
 del_file:
-	mov si, entire_command
-	mov ax, 4
-	add si, ax
-	mov di, input_buffer_copy
-	find_file_loop:
-		cmp byte [si], 0
-		je .done
-		mov bl, [si]
-		mov byte [di], bl
-		inc si
-		inc di
-		jmp find_file_loop
-	.done:
-	mov ax, input_buffer_copy
 	
+	mov ax, input_buffer
+	add ax, 4
 	call os_file_exists
 	jc no_file
+	
 	call os_remove_file
 	jmp second
 shutdown_do:
 	call clear
 	mov si, shutdown_msg
-	call print
+	call os_print_string
 	jmp $
 	
 reboot_do:
@@ -646,7 +612,7 @@ ren_do:
 	
 help_do:
 	mov si, help_msg
-	call print
+	call os_print_string
 	jmp second
 
 size_wise:
@@ -672,9 +638,9 @@ size_wise:
 	mov ax, bx
 	call os_int_to_string
 	mov si, ax
-	call print
+	call os_print_string
 	mov si, ending_size
-	call print
+	call os_print_string
 	
 	jmp second
 
@@ -685,12 +651,12 @@ clear_screen:
 	
 no_file:
 	mov si, no_file_msg
-	call print
+	call os_print_string
 	jmp second
 	
 no_kernel:
 	mov si, no_execute_kernel
-	call print
+	call os_print_string
 	jmp second
 	
 load_program:	
@@ -702,7 +668,7 @@ load_program:
 	int 0x10
 	mov al, 0x0D
 	int 0x10
-	mov si, entire_command
+	mov si, input_buffer
 	find_param:
 	lodsb
 	cmp al, 0
@@ -728,31 +694,14 @@ no_param:
 
 failed:
 	mov si, skibidi
-	call print
+	call os_print_string
 	jmp second
-	
-print:
-	pusha
 
-	mov ah, 0Eh			; int 10h teletype function
-
-.repeat:
-	lodsb				; Get char from string
-	cmp al, 0
-	je .done			; If char is zero, end of string
-
-	int 10h				; Otherwise, print it
-	jmp .repeat			; And move on to next char
-
-.done:
-	popa
-	ret
 	
 list_directory_thing:
-	
+	mov byte [dirlist], 0
 	call list_directory
-	
-	
+
 	jmp second
 
 delay:
@@ -770,7 +719,7 @@ wait_loop:
 	
 no_modify_kernel:
 	mov si, nice_try
-	call print
+	call os_print_string
 	jmp second
 
 no_txt_msg db 10, 13, 'You cannot run a text file', 0
@@ -781,7 +730,6 @@ unkown_command db 10, 13,'Unknown command', 0
 no_execute_kernel db 10, 13,'You cannot execute KERNEL.BIN,', 10, 13, 'as it is a core file and will', 10, 13, 'make TommyOS unusable!', 0
 nice_try db 10, 13, 'Nice try.', 0
 no_file_msg db 10, 13,'File not found or none specified.', 0
-help_msg db 10, 13, 'Availible commands are:', 10, 13, 'DIR, TIME, DATE, CLS, ECHO,', 10, 13, 'SIZE, SHUTDOWN, REBOOT', 10, 13, 'DEL, REN, COPY, HELP', 0
 skibidi db 10, 13, 'Errored', 0 ;what was this for?
 txt_extension db 'TXT', 0
 tom_extension db 'TOM', 0
@@ -791,17 +739,18 @@ floppy_one db 'A:', 0
 ending_size db ' bytes', 0
 prompt db 10, 13, '> ', 0
 ;commands
-dir_str db 'DIR', 0 ;doTe
-time_str db 'TIME', 0 ;done
-date_str db 'DATE', 0 ;done 
-cls_str db 'CLS', 0 ;done
-echo_str db 'ECHO', 0 ;done
-size_str db 'SIZE', 0 ;done
-shutdown_str db 'SHUTDOWN', 0 ;done
-reboot_str db 'REBOOT', 0 ;done
-del_str db 'DEL', 0 ;done
-ren_str db 'REN', 0 ;done
-help_str db 'HELP', 0 ;done
+help_msg db 10, 13, 'Availible commands are:', 254, 0
+dir_str db 'DIR', 0, 255 ;doTe
+time_str db 'TIME', 0, 255 ;done
+date_str db 'DATE', 0, 255 ;done 
+cls_str db 'CLS', 0, 255 ;done
+echo_str db 'ECHO', 0, 255 ;done
+size_str db 'SIZE', 0, 255 ;done
+shutdown_str db 'SHUTDOWN', 0, 255 ;done
+reboot_str db 'REBOOT', 0, 255 ;done
+del_str db 'DEL', 0, 255 ;done
+ren_str db 'REN', 0, 255 ;done
+help_str db 'HELP', 0, 255 ;done
 copy_str db 'COPY', 0
 kernel_iden_two db 'KERNEL'
 	
@@ -810,7 +759,7 @@ drive_set_to_b db 10, 13,'Drive set to B', 0
 no_drive_msg db 10, 13, 'Drive not present', 0
 
 
-entire_command times 60 db 0
+
 tmp_one dw 0
 
 previous_file_run times 13 db 0
@@ -818,7 +767,7 @@ input_buffer times 60 db 0
 input_buffer_copy times 60 db 0
 input_buffer_copy_copy times 60 db 0
 
-dirlist			times 1024 db 0
+
 	tmp_string		times 15 db 0
 
 	file_size		dw 0
@@ -833,8 +782,10 @@ shutdown_msg db 'You can now turn off your machine.', 0
 msg2 db "V1.1", 10, 13, 0
 
 skiplines dw 0
-sector_buffer times 512 db 0 ;this is so we can check if a drive is loaded in or not, sample read and save it here
+;sector_buffer times 512 db 0 ;this is so we can check if a drive is loaded in or not, sample read and save it here
 %include "./extra/functions.asm"
 %include "./extra/old_funcs.asm"
 disk_buffer equ 24576
+dirlist:
+sector_buffer:
 buffer:
